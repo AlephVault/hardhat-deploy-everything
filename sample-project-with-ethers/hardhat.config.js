28c7aeda48b579599f12d62860f085b4884d3d36
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-11 22:40:35 -0500

    Adding the dependencies into the sample projects.

--- sample-project-with-ethers/hardhat.config.js
+++ sample-project-with-ethers/hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-enquirer-plus");
+require("hardhat-common-tools");
+require("hardhat-blueprints");
+require("hardhat-ignition-deploy-everything");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
--- sample-project-with-viem/hardhat.config.ts
+++ sample-project-with-viem/hardhat.config.ts
@@ -1,5 +1,9 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "hardhat-enquirer-plus";
+import "hardhat-common-tools";
+import "hardhat-blueprints";
+import "hardhat-ignition-deploy-everything";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
